3a7430086cad4529bc04083f857e27321dc21d73
Nogoza <151651571+Nogoza@users.noreply.github.com>
2026-02-03 21:57:09 +0300

    Add SQL queries for film and payment analysis
    Added SQL queries to analyze film lengths, rental rates, and customer purchases in the dvdrental database.

---
+++ PostgreSQL_HomeWork_12.sql
@@ -0,0 +1,61 @@
+/*Ödev 12
+Merhabalar,
+Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz.
+
+film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+Kolay Gelsin.*/
+
+--film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+
+SELECT COUNT(*) AS film_sayisi
+FROM film
+WHERE length >
+(
+SELECT AVG(length)
+FROM film
+);
+
+--film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+
+SELECT COUNT(*)
+FROM film
+WHERE rental_rate =
+(
+SELECT MAX(rental_rate)
+FROM film
+)
+
+--film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+
+SELECT *
+FROM film
+WHERE rental_rate =
+(
+SELECT MIN(rental_rate)
+FROM film
+)
+AND
+replacement_cost =
+(
+SELECT MIN(replacement_cost)
+FROM film
+)
+
+--payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+
+SELECT customer_id, COUNT(*) AS alisveris_sayisi
+FROM payment
+GROUP BY customer_id
+HAVING COUNT(*) = (
+    SELECT MAX(alisveris_sayisi)
+    FROM (
+        SELECT COUNT(*) AS alisveris_sayisi
+        FROM payment
+        GROUP BY customer_id
+    ) t
+)
+ORDER BY customer_id;
